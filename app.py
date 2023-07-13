65d87f81fc260993c1c58a2dafaeb433e295a6f7
diegonadas <di.ariasf@duocuc.cl>
2023-07-12 23:24:50 -0400

    tienda

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/producto/<int:producto_id>')
+def mostrar_producto(producto_id):
+    producto = {
+        'id': producto_id,
+        'nombre': 'Producto',
+        'precio': 9.99,
+        'descripcion': 'ejemplo de producto'
+    }
+    return render_template('producto.html', producto=producto)
+
+if __name__ == '__main__':
+    app.run()
