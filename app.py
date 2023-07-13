1bc9ec6e6f7685416382e673a2c25a52383de387
Diego <136105077+diegonadas@users.noreply.github.com>
2023-07-13 10:05:23 -0400

    final update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask import Flask, jsonify
+import request
 
 app = Flask(__name__)
 
@@ -16,6 +17,14 @@ def mostrar_producto(producto_id):
         'descripcion': 'ejemplo de producto'
     }
     return render_template('producto.html', producto=producto)
+@app.route('/')
+def index():
+    response = requests.get('https://musicpro.bemtorres.win/api/v1/bodega/producto')
+    if response.status_code == 200:
+        data = response.json()
+        return jsonify(data)
+    else:
+        return 'Error al obtener datos de la API'
 
 if __name__ == '__main__':
     app.run()
